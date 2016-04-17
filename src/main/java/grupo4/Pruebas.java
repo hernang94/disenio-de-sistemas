73043bb251bf23d89e8a8cb7c644187e0b8faba7
hernang94 <hernangallo@outlook.com>
2016-04-17 12:44:12 -0300

    Clase prueba con el manejo de horarios a traves de LocalDate

---
+++ src/main/java/grupo4/Pruebas.java
@@ -0,0 +1,23 @@
+package grupo4;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetTime;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LocalTime fechainicio= LocalTime.of(11, 55);
+		LocalTime fechainicio2= LocalTime.of(11, 54);
+		if(fechainicio2.getMinute()<fechainicio.getMinute()){
+			System.out.println("Eureka(?");
+			System.out.println(fechainicio);
+			System.out.println(fechainicio2);
+		}
+		else{
+		System.out.println(fechainicio);
+		}
+	}
+
+}
