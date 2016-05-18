473187865a9bb6c195c5c239834cd26182c9e154
CMauro96 <corvaro.mauro@gmail.com>
2016-05-17 22:20:22 -0300

    cambios en el transformer de JSon

--- src/main/java/grupo4/BancoTransformer.java
+++ src/main/java/grupo4/BancoTransformer.java
@@ -1,8 +1,5 @@
 package grupo4;
 
-import java.io.IOException;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BancoTransformer {
@@ -11,19 +8,9 @@ public class BancoTransformer {
 
 	public BancoTransformer() {
 		this.objectMapper = new ObjectMapper();
-		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 	}
 	
 	public ObjectMapper getObjectMapper() {
 		return objectMapper;
-	}
-	
-	public <T> T fromJson(String json, Class<T> typeReference) {
-		try {
-			return this.objectMapper.readValue(json, typeReference);
-		} catch (IOException e) {
-
-			throw new RuntimeException("Error al leer de JSON", e);
-		}
 	}
 }
