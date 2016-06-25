504c529827fa1aefea0c16e032668f0410205230
hernang94 <hernangallo@outlook.com>
2016-06-25 15:48:19 -0300

    Correcion de Cobertura

--- src/test/java/Tests/AccionesConfigurablesTest.java
+++ src/test/java/Tests/AccionesConfigurablesTest.java
@@ -214,7 +214,7 @@ public class AccionesConfigurablesTest {
 		Assert.assertEquals(10, dispositivoTactil.calcularDiferencia(LocalDateTime.of(2016, 06, 05, 18, 15, 10), LocalDateTime.of(2016, 06, 05, 18, 15, 20)));
 	}
 	
-	/*@Test
+	@Test
 	public void reportarCantidadBusquedas(){
 		dispositivoTactil.busquedaLibre("muebleria");
 		dispositivoTactil.obtenerReporteTotalPorFecha();
@@ -233,5 +233,5 @@ public class AccionesConfigurablesTest {
 		dispositivoTactil.busquedaLibre("muebleria");
 		repo.reporteParcialporTerminal();
 		Mockito.verify(writer).println("Usuario: terminalAbasto");
-	}*/
+	}
 }
