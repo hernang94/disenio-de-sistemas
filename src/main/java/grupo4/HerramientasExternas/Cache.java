68dd82202815b277db5e76f82f4a4913c9c2ae1d
Mauro Corvaro <corvaro.mauro@gmail.com>
2016-10-15 18:13:18 -0300

    Cache y Redis terminadas, falta testear funcionalidad

--- src/main/java/grupo4/HerramientasExternas/Cache.java
+++ src/main/java/grupo4/HerramientasExternas/Cache.java
@@ -7,19 +7,24 @@ import grupo4.POIs.Poi;
 import redis.clients.jedis.Jedis;
 
 public class Cache {
-	private Jedis jedis = new Jedis();
-	private static Cache instancia = new Cache();
+	private static Jedis jedis = new Jedis();
+	private static Cache instancia;
+	//Por Jedis equivale a segundos
+	private static int duracionXKey= 60;
 	
 	public static Cache getInstancia() {
+		if(instancia==null){
+			instancia= new Cache();
+		}
 		return instancia;
 	}
-	
+
 	public boolean criterioEstaEnCache(String criterio){
 		return jedis.exists(criterio);
 	}
 	
 	public void actualizarCache(String key,String value){
-		jedis.set(key, value);
+		jedis.setex(key,duracionXKey, value);
 	}
 	
 	public List<Poi> obtenerPois(String criterio){
