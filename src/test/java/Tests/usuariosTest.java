86c471ff3b952cbafbb442079d2299d0a9094303
Hernan Gallo <hernangallo@outlook.com>
2016-11-14 02:39:31 -0300

    Agrego pequeño test para testear la funcionalidad de los usuarios y su logueo

---
+++ src/test/java/Tests/usuariosTest.java
@@ -0,0 +1,42 @@
+package Tests;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
+import grupo4.Acciones.Usuario;
+import grupo4.POIs.Users;
+import grupo4.Repositorios.RepositorioCuentas;
+
+
+public class usuariosTest extends AbstractPersistenceTest implements WithGlobalEntityManager{
+	private Users cuenta1;
+	private Usuario terminal2;
+	@Before
+	public void init(){
+		terminal2=new Usuario("Mataderos", 9);
+		entityManager().persist(terminal2);
+		cuenta1= new Users("Ricardo", "Fort",terminal2);
+		RepositorioCuentas.getInstancia().agregarUsuario(cuenta1);
+		//entityManager().persist(cuenta1);
+		entityManager().flush();
+		entityManager().clear();
+	}
+	@Test
+	public void testPrueboTraerElUnicoUsuarioExistente(){
+		Users cuenta2= RepositorioCuentas.getInstancia().buscarUsuario("Ricardo", "Fort");
+		Assert.assertTrue(cuenta2!=null);
+	}
+	@Test
+	public void testPrueboSiTraeAlgo(){
+		List<Users> listaAux=entityManager().createQuery("from Users", Users.class).getResultList();
+		Assert.assertTrue(!listaAux.isEmpty());
+	}
+
+}
