22a61f143a45362cd1d4e29903861fe79d7f16bc
Hernan Gallo <hernangallo@outlook.com>
2016-11-16 04:11:54 -0300

    Agrego nuevas funcionalidades al controller, pero va a haber que separarlo en varios

--- src/main/java/controllers/Controller.java
+++ src/main/java/controllers/Controller.java
@@ -1,10 +1,21 @@
 package controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-import grupo4.POIs.Users;
+import javax.persistence.EntityManager;
+
+import org.uqbarproject.jpa.java8.extras.PerThreadEntityManagers;
+
+import grupo4.Acciones.Usuario;
+import grupo4.POIs.Poi;
 import grupo4.Repositorios.RepositorioCuentas;
+import grupo4.Repositorios.RepositorioDePois;
+import grupo4.Repositorios.RepositorioDeUsuarios;
+import grupo4.Usuarios.Users;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
@@ -21,15 +32,22 @@ public class Controller {
 		u1= RepositorioCuentas.getInstancia().buscarUsuario(req.queryParams("usuario").toString(), req.queryParams("contrasenia").toString());
 		
 		if (u1!=null){
-			res.redirect("/principalAdmin");
+			if(u1.getDecriminatorValue().equalsIgnoreCase("ADMINISTRADOR"))
+			res.redirect("/administrador/principal");
+			else{
+				res.redirect("/terminal/principal");
+			}
 		}
 		res.redirect("/logueoIncorrecto");
 		return null;
 	}
+	public ModelAndView mostrarPantallaParaListarPois(Request req, Response res){
+		return new ModelAndView(null,"Administrador/formListarPois.hbs");
+	}
 	public  ModelAndView mostrarPrincipalAdmin(Request req, Response res){
 		Map<String, Users> model= new HashMap<String, Users>();
 		model.put("user", u1);
-		return new ModelAndView(model, "principalAdmin.hbs");
+		return new ModelAndView(model, "Administrador/principalAdmin.hbs");
 	}
 	public  ModelAndView mostrarLogueoIncorrecto(Request req, Response res){
 		return new ModelAndView(null, "logueoIncorrecto.hbs");
@@ -42,7 +60,50 @@ public class Controller {
 	public  ModelAndView mostrarPrincipalTerminal(Request req, Response res){
 		Map<String, Users> model= new HashMap<String, Users>();
 		model.put("user", u1);
-		return new ModelAndView(model, "principalTerminal.hbs");
+		return new ModelAndView(model, "Terminal/principalTerminal.hbs");
 	}	
-
+	public ModelAndView listarPois(Request req, Response res){
+		List<Poi> pois=evaluarRetorno(req.queryParams("nombrePoi").toString(), req.queryParams("Tipos").toString());
+		Map<String, List<Poi>> model=new HashMap<>();
+		model.put("pois", pois);
+		return new ModelAndView(model, "Administrador/listadoPoisAdmin.hbs");
+	}
+	
+	public ModelAndView mostrarPantallaParaListarTerminales(Request req, Response res){
+		return new ModelAndView(null,"Administrador/formListarTerminales.hbs");
+	}
+	public ModelAndView listarTerminales(Request req, Response res){
+		List<Usuario> terminales;
+		if(req.queryParams("comunas").equalsIgnoreCase("Todas")){
+			terminales=RepositorioDeUsuarios.getInstancia().getListaDeUsuarios();
+		}
+		else{
+			terminales=RepositorioDeUsuarios.getInstancia().getListaDeUsuariosXComuna(Integer.parseInt(req.queryParams("comunas").toString()));
+		}
+		Map<String, List<Usuario>> model=new HashMap<>();
+		model.put("terminales", terminales);
+		return new ModelAndView(model, "Administrador/listadoTerminales.hbs");
+	}
+	public ModelAndView mostrarBusquedaPois(Request req, Response res){
+		return new ModelAndView(null, "Terminal/buscarPois.hbs");
+	}
+	public ModelAndView buscarPoisTerminal(Request req, Response res){
+		List<Poi> pois=RepositorioDePois.getInstancia().busquedaLibre(req.queryParams("criterio").toString());
+		Map<String, List<Poi>> model=new HashMap<>();
+		model.put("pois", pois);
+		return new ModelAndView(model, "Terminal/mostrarResultadosBusqueda.hbs");
+	}
+	
+private List<Poi> evaluarRetorno(String nombre,String tipo){
+	/*if(nombre.isEmpty()&&tipo.isEmpty()){
+		return null;
+	}*/
+	if(tipo.equalsIgnoreCase("Todos")){
+		return RepositorioDePois.getInstancia().busquedaPorNombre(nombre);
+	}
+	if(nombre.isEmpty()){
+		return RepositorioDePois.getInstancia().busquedaPorTipo(tipo);
+	}
+		return RepositorioDePois.getInstancia().busquedaPorNombre(nombre).stream().filter(poi->poi.getTipo().equalsIgnoreCase(tipo)).collect(Collectors.toList());
+}
 }
